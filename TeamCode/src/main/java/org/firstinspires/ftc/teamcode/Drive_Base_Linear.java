db9d71074a09442ba7ee6693f008d6c153d22ed5
Ulban Adhikary <uadhi2@eq.edu.au>
2020-03-16 13:14:32 +1000

    Fixed controls

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive_Base_Linear.java
@@ -0,0 +1,124 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+@TeleOp(name="Drive_Bas_Linear", group="Linear Opmode")
+//@Disabled
+public class Drive_Base_Linear extends LinearOpMode {
+
+    // Declare OpMode members
+    private ElapsedTime runtime = new ElapsedTime();
+    // 152 rpm
+    private DcMotor frontLeftDrive = null;
+    // 152 rpm
+    private DcMotor frontRightDrive = null;
+    // 100 rpm
+    private DcMotor backLeftDrive = null;
+    // 100 rpm
+    private DcMotor backRightDrive = null;
+
+    private double motorWeight = 0.65789473684;
+
+    private double reverseControls = 1;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        frontLeftDrive  = hardwareMap.get(DcMotor.class, "front_left_drive");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
+        backLeftDrive  = hardwareMap.get(DcMotor.class, "back_left_drive");
+        backRightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
+
+
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // setup the inputs
+            double  G1LeftStickY  = reverseControls * gamepad1.left_stick_y;
+            double  G1RightStickY = reverseControls * gamepad1.right_stick_y;
+            double  G1LeftStickX  = reverseControls * gamepad1.left_stick_x;
+            double  G1RightStickX = reverseControls * gamepad1.right_stick_x;
+
+            // strafe Mode (allows sideways motion)
+            backRightDrive.setPower(G1LeftStickX + G1RightStickX + G1LeftStickY);
+            backLeftDrive.setPower(G1LeftStickX + G1RightStickX - G1LeftStickY);
+            frontLeftDrive.setPower(G1LeftStickX - G1RightStickX + G1LeftStickY);
+            frontRightDrive.setPower(G1LeftStickX - G1RightStickX - G1LeftStickY);
+
+            // Reverse controls with the x button
+            if (gamepad1.x){
+                reverseControls = -reverseControls;
+                telemetry.addData("Controls Status", "Direction: " + reverseControls);
+            }
+
+            // SLOW mode and FAST mode. Works by halving the reverseControls variable
+            if (gamepad1.y){
+                if(reverseControls == 1 || reverseControls == -1){
+                    reverseControls = reverseControls*0.5;
+                    telemetry.addData("Controls Status", "SLOW MODE");
+                }
+                else if(reverseControls == 0.5 || reverseControls == -0.5){
+                    reverseControls = reverseControls*2;
+                    telemetry.addData("Controls Status", "FAST MODE");
+                }
+                telemetry.update();
+            }
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
+}
